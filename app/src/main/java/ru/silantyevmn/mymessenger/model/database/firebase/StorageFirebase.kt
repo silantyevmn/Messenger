09290f5ee2705c6843601851cab9c16dbf87bb96
silantyevmn <5992033@mail.ru>
2019-02-05 19:33:37 +0300

    почистил ненужный import добавил onComplete

--- app/src/main/java/ru/silantyevmn/mymessenger/model/database/firebase/StorageFirebase.kt
+++ app/src/main/java/ru/silantyevmn/mymessenger/model/database/firebase/StorageFirebase.kt
@@ -1,28 +1,16 @@
 package ru.silantyevmn.mymessenger.model.database.firebase
 
-import android.content.Intent
 import android.net.Uri
 import android.util.Log
-import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
-import io.reactivex.Completable
 import io.reactivex.Observable
-import ru.silantyevmn.mymessenger.model.cache.UserCache
 import ru.silantyevmn.mymessenger.model.database.IStorageDatabase
-import ru.silantyevmn.mymessenger.model.entity.User
-import ru.silantyevmn.mymessenger.ui.MessengerActivity
-import java.lang.RuntimeException
 import java.util.*
 
 
 class StorageFirebase : IStorageDatabase {
     override fun putFile(fileUri: Uri): Observable<String> {
-        return Observable.create{emitter->
+        return Observable.create { emitter ->
             if (fileUri == null) emitter.onError(RuntimeException("File is null"))
             val fileName = UUID.randomUUID().toString()
             val ref = FirebaseStorage.getInstance().getReference("/images/$fileName")
@@ -32,6 +20,7 @@ class StorageFirebase : IStorageDatabase {
                     ref.downloadUrl.addOnSuccessListener {
                         Log.d("putFile", "File location: $it")
                         emitter.onNext(it.toString())
+                        emitter.onComplete()
                     }
                 }
                 .addOnFailureListener {
