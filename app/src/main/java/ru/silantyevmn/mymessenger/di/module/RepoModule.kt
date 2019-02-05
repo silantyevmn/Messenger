8e0f7d6e9f37dce26e3a3f232d240ab0b971376a
silantyevmn <5992033@mail.ru>
2019-02-05 19:48:50 +0300

    добавил cacheModule и в репо добавил IchatCache,IUserCache

--- app/src/main/java/ru/silantyevmn/mymessenger/di/module/RepoModule.kt
+++ app/src/main/java/ru/silantyevmn/mymessenger/di/module/RepoModule.kt
@@ -2,20 +2,27 @@ package ru.silantyevmn.mymessenger.di.module
 
 import dagger.Module
 import dagger.Provides
-import ru.silantyevmn.mymessenger.di.module.DatabaseModule
+import ru.silantyevmn.mymessenger.model.cache.IChatCache
+import ru.silantyevmn.mymessenger.model.cache.IUserCache
 import ru.silantyevmn.mymessenger.model.database.IStorageDatabase
 import ru.silantyevmn.mymessenger.model.database.firebase.*
 import ru.silantyevmn.mymessenger.model.repo.IRepo
 import ru.silantyevmn.mymessenger.model.repo.Repo
 import javax.inject.Singleton
 
-@Module(includes = (arrayOf(DatabaseModule::class)))
+@Module(includes = (arrayOf(DatabaseModule::class,CacheModule::class)))
 class RepoModule {
 
     @Singleton
     @Provides
-    fun provideRepo(userDatabase: IUserDatabase, chatDatabase: IChatDatabase,authDatabase: IAuthUser,storageDatabase: IStorageDatabase): IRepo {
-        return Repo(userDatabase, chatDatabase,authDatabase,storageDatabase)
+    fun provideRepo(userDatabase: IUserDatabase,
+                    chatDatabase: IChatDatabase,
+                    authDatabase: IAuthUser,
+                    storageDatabase: IStorageDatabase,
+                    chatCache: IChatCache,
+                    userCache: IUserCache
+    ): IRepo {
+        return Repo(userDatabase, chatDatabase,authDatabase,storageDatabase,chatCache,userCache)
     }
 
 }
