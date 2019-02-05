4e4b6b2b4772aab670658be10aae10904a73e014
silantyevmn <5992033@mail.ru>
2019-02-05 19:50:52 +0300

    добавил CacheModule

--- app/src/main/java/ru/silantyevmn/mymessenger/di/AppComponent.kt
+++ app/src/main/java/ru/silantyevmn/mymessenger/di/AppComponent.kt
@@ -1,9 +1,11 @@
 package ru.silantyevmn.mymessenger.di
 
 import dagger.Component
+import ru.silantyevmn.mymessenger.di.module.CacheModule
 import ru.silantyevmn.mymessenger.di.module.DatabaseModule
+import ru.silantyevmn.mymessenger.di.module.ImageModule
 import ru.silantyevmn.mymessenger.di.module.RepoModule
-import ru.silantyevmn.mymessenger.ui.LoginActivity
+import ru.silantyevmn.mymessenger.ui.login.LoginActivity
 import ru.silantyevmn.mymessenger.ui.MessengerActivity
 import ru.silantyevmn.mymessenger.ui.NewMessengerActivity
 import ru.silantyevmn.mymessenger.ui.RegisterActivity
@@ -11,7 +13,7 @@ import ru.silantyevmn.mymessenger.ui.chat.ChatLogActivity
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = arrayOf(DatabaseModule::class, RepoModule::class))
+@Component(modules = arrayOf(DatabaseModule::class, RepoModule::class,CacheModule::class,ImageModule::class))
 interface AppComponent {
     fun inject(chatActivity: ChatLogActivity)
     fun inject(messengerActivity: MessengerActivity)
