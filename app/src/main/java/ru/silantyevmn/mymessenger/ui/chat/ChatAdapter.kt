570d3749db47d25aff6bc336aac2769b268282f5
silantyevmn <5992033@mail.ru>
2019-02-05 19:54:20 +0300

    добавил функционал отображение фото в чате

--- app/src/main/java/ru/silantyevmn/mymessenger/ui/chat/ChatAdapter.kt
+++ app/src/main/java/ru/silantyevmn/mymessenger/ui/chat/ChatAdapter.kt
@@ -1,34 +1,70 @@
 package ru.silantyevmn.mymessenger.ui.chat
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
+import com.squareup.picasso.Callback
+import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
-import ru.silantyevmn.mymessenger.utils.DateTimeManager
 import ru.silantyevmn.mymessenger.R
 import ru.silantyevmn.mymessenger.model.entity.ChatMessage
+import ru.silantyevmn.mymessenger.ui.image.ImageLoader
+import ru.silantyevmn.mymessenger.utils.DateTimeManager
+import java.lang.Exception
 
-class ChatAdapter(val presenter: ChatPresenter) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+class ChatAdapter(val presenter: ChatPresenter,val imageLoader: ImageLoader) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     inner class ViewHolderRight(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val message: TextView
         private val time: TextView
-        private val image: CircleImageView
+        private val imageUser: CircleImageView
         private val status: ImageView
+        private val messageImage:ImageView
+        private val messageImageLayout: RelativeLayout
 
         init {
-            message = itemView.findViewById(R.id.chat_item_right_messages)
+            message = itemView.findViewById(R.id.chat_item_right_messages_text)
             time = itemView.findViewById(R.id.chat_item_right_time)
-            image = itemView.findViewById(R.id.chat_item_right_image)
+            imageUser = itemView.findViewById(R.id.chat_item_right_image)
             status = itemView.findViewById(R.id.chat_item_right_status)
+            messageImage = itemView.findViewById(R.id.chat_item_right_message_image_image)
+            messageImageLayout = itemView.findViewById(R.id.chat_item_right_message_image)
         }
 
         fun bind(chatMessage: ChatMessage) {
-            message.text = chatMessage.text
+            if (chatMessage.typeMessage.equals("text")) {
+                message.text = chatMessage.message
+                message.visibility = View.VISIBLE
+                messageImageLayout.visibility = View.GONE
+            } else if (chatMessage.typeMessage.equals("image")) {
+                message.visibility = View.GONE
+                messageImageLayout.visibility = View.VISIBLE
+
+                imageLoader.showImage(chatMessage.message,messageImage)
+                /*Picasso.get().load(chatMessage.message)
+                    .placeholder(R.drawable.placeholder)
+                    .networkPolicy(NetworkPolicy.OFFLINE)
+                    .into(messageImage,object :Callback{
+                        override fun onSuccess() {
+                            Log.d("Piccasso","onSuccess")
+                        }
+
+                        override fun onError(e: Exception?) {
+                            Log.d("Piccasso","onError ${e.toString()}")
+                            Picasso.get().load(chatMessage.message)
+                                .error(R.drawable.placeholder)
+                                .into(messageImage)
+                        }
+
+                    })*/
+            }
             time.text = DateTimeManager().getTime(chatMessage.time)
             when (chatMessage.status) {
                 0 -> {
@@ -36,27 +72,41 @@ class ChatAdapter(val presenter: ChatPresenter) : RecyclerView.Adapter<RecyclerV
                 1 -> status.setImageResource(R.drawable.ic_done_black_24dp)
                 2 -> status.setImageResource(R.drawable.ic_done_all_black_24dp)
             }
-            Picasso.get().load(presenter.currentUser.imagePhotoUri)
+            imageLoader.showImage(presenter.currentUser.imagePhotoUri,imageUser)
+            /*Picasso.get().load(presenter.currentUser.imagePhotoUri)
                 .placeholder(R.drawable.placeholder)
-                .into(image)
+                .into(imageUser)*/
         }
     }
 
     inner class ViewHolderLeft(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val message: TextView
         private val time: TextView
-        private val image: CircleImageView
+        private val imageUser: CircleImageView
         private val status: ImageView
+        private val messageImage:ImageView
 
         init {
             message = itemView.findViewById(R.id.chat_item_left_messages)
             time = itemView.findViewById(R.id.chat_item_left_time)
-            image = itemView.findViewById(R.id.chat_item_left_image)
+            imageUser = itemView.findViewById(R.id.chat_item_left_image)
             status = itemView.findViewById(R.id.chat_item_left_status)
+            messageImage = itemView.findViewById(R.id.chat_item_left_message_image)
         }
 
         fun bind(chatMessage: ChatMessage) {
-            message.text = chatMessage.text
+            if (chatMessage.typeMessage.equals("text")) {
+                message.text = chatMessage.message
+                message.visibility = View.VISIBLE
+                messageImage.visibility = View.GONE
+            } else if (chatMessage.typeMessage.equals("image")) {
+                message.visibility = View.GONE
+                messageImage.visibility = View.VISIBLE
+                imageLoader.showImage(chatMessage.message,messageImage)
+                /*Picasso.get().load(chatMessage.message)
+                    .placeholder(R.drawable.placeholder)
+                    .into(messageImage)*/
+            }
             time.text = DateTimeManager().getTime(chatMessage.time)
             when (chatMessage.status) {
                 0 -> {
@@ -64,14 +114,11 @@ class ChatAdapter(val presenter: ChatPresenter) : RecyclerView.Adapter<RecyclerV
                 1 -> status.setImageResource(R.drawable.ic_done_black_24dp)
                 2 -> status.setImageResource(R.drawable.ic_done_all_black_24dp)
             }
-            Picasso.get().load(presenter.toUser.imagePhotoUri)
+            imageLoader.showImage(presenter.toUser.imagePhotoUri,imageUser)
+            /*Picasso.get().load(presenter.toUser.imagePhotoUri)
                 .placeholder(R.drawable.placeholder)
-                .into(image)
+                .into(imageUser)*/
         }
-    }
-
-    fun updateMessage(position: Int) {
-        notifyItemChanged(position)
     }
 
     override fun getItemViewType(position: Int): Int {
