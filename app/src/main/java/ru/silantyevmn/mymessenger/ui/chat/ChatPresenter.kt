453d6f7c34b9bb7e08e9e316f7afa82bd0566c7f
silantyevmn <5992033@mail.ru>
2019-02-05 19:56:29 +0300

    Добавил функционал отображение фото в чате, обработка и сохранение фото в базе и интернете

--- app/src/main/java/ru/silantyevmn/mymessenger/ui/chat/ChatPresenter.kt
+++ app/src/main/java/ru/silantyevmn/mymessenger/ui/chat/ChatPresenter.kt
@@ -1,22 +1,23 @@
 package ru.silantyevmn.mymessenger.ui.chat
 
+import android.graphics.Bitmap
+import android.net.Uri
 import android.util.Log
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.RemoteMessage
 import io.reactivex.android.schedulers.AndroidSchedulers
-import ru.silantyevmn.mymessenger.di.App
-import ru.silantyevmn.mymessenger.model.cache.UserCache
-import ru.silantyevmn.mymessenger.model.database.firebase.ChatFirebase
-import ru.silantyevmn.mymessenger.model.database.firebase.IUserDatabase
+import ru.silantyevmn.mymessenger.model.FileManager
 import ru.silantyevmn.mymessenger.model.entity.ChatMessage
+import ru.silantyevmn.mymessenger.model.entity.FactoryChatMessage
 import ru.silantyevmn.mymessenger.model.entity.User
-import ru.silantyevmn.mymessenger.model.database.firebase.UserFirebase
 import ru.silantyevmn.mymessenger.model.repo.IRepo
-import ru.silantyevmn.mymessenger.model.repo.Repo
-import javax.inject.Inject
 
 @InjectViewState
-class ChatPresenter(val repo: IRepo,val currentUserUid: String, val toUserUid: String) :
+class ChatPresenter(val repo: IRepo, val currentUserUid: String, val toUserUid: String) :
     MvpPresenter<ChatView>() {
 
     lateinit var currentUser: User
@@ -42,59 +43,98 @@ class ChatPresenter(val repo: IRepo,val currentUserUid: String, val toUserUid: S
     }
 
     fun loadChat() {
+        repo.loadMessageList(currentUserUid,toUserUid)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                chatList = it as ArrayList<ChatMessage>
+                viewState.updateAdapter()
+                viewState.setScrollRecycler()
+                viewState.onSuccess()
+            }, {
+                viewState.onError(it.message.toString())
+            })
+
+        //подпишемся на сообщения
         repo.loadMessage(currentUserUid, toUserUid)
             .observeOn(AndroidSchedulers.mainThread())
-            .doOnNext {
+            .subscribe({
                 if (it.toUid.equals(currentUserUid) && it.status == 1) {
                     var tempChat = it
                     tempChat.status = 2 //меняю на статус получено
-                    repo.updateMessage(tempChat).subscribe()//обновляю в базе
+                    if(!it.uid.isEmpty()) {
+                        repo.updateMessage(tempChat).subscribe()//обновляю в базе
+                    }
+                    Log.d("Test","it.toUid.equals(currentUserUid) && it.status == 1 message ${it.message}")
                 }
                 if (chatList.contains(it)) {
-                    Log.d("Chat_presenter", " true ${it.id}")
                     var position = chatList.indexOf(it)
-                    chatList.set(position, it)
-                    viewState.updateStatus(position)
+                    Log.d("Test","chatList.contains(it)2 message ${chatList.get(position).message}")
+                    //меняем только статус
+                    if (it.status == 2) {
+                        chatList[position].status = it.status
+                        viewState.updateAdapter(position)
+                    }
+                    //chatList.set(position, it)
+                    //viewState.updateAdapter(position)
                     //нужно отловить статус
                 } else {
+                    Log.d("Test","else message ${it.message}")
                     chatList.add(it)
                     viewState.updateAdapter()
                 }
-
-            }
-            .subscribe({
                 viewState.setScrollRecycler()
                 viewState.onSuccess()
-                //chatView.setAdapter(chatMessage)
             }, {
                 viewState.onError(it.message.toString())
             })
     }
 
-    fun pushMessages(textMessage: String, currentTimeMillis: Long) {
-        var status = 1 //статус отправки
-        val chatMessage = ChatMessage(
-            "",
-            currentUserUid,
-            toUserUid,
-            textMessage,
-            currentTimeMillis,
-            status
-        )
-        viewState.setTextMessagesClear()
-/*        chatList.add(chatMessage)
-        chatView.updateAdapter()
-        chatView.setTextMessagesClear()
-        chatView.setScrollRecycler()*/
+    fun pushMessages(textMessage: String) {
+        var tempChatMessage = FactoryChatMessage().createChatMessage(currentUserUid, toUserUid, "text", textMessage, 1)
+        chatList.add(tempChatMessage)
+        viewState.updateAdapter()
+        viewState.setTextMessagesClear()//очищяем текст
+        viewState.setScrollRecycler()
 
-        //chatView.showLoading("Сохранение данных...")
-        repo.pushMessage(chatMessage)
+        //указать статус загрузки на фото
+        repo.pushMessage(tempChatMessage)
             .subscribe({
-               /* chatMessage.status = 1
-                repo.updateMessages(chatMessage).subscribe()*/
+                //снять статус загрузки
             }, {
                 viewState.onError(it.message.toString())
             })
+    }
+
+    fun onClickImageLoad() {
+        viewState.showLoadImage()
+    }
+
+    fun pushImage(uri: Uri) {
+        //сохраняем в адаптер и отображаем
+        val tempChatMessage =
+            FactoryChatMessage().createChatMessage(currentUserUid, toUserUid, "image", uri.toString(), 1)
+        chatList.add(tempChatMessage);
+        Log.d("Test","записал 1 раз message ${tempChatMessage.message}")
+        viewState.updateAdapter()
+        viewState.setScrollRecycler()
+
+        //сохраняем файл на сервере
+        repo.putFile(uri)
+            .subscribe({
+                tempChatMessage.message = it
+                repo.pushMessage(tempChatMessage)
+                    .subscribe({
+                        //загрузили
+                    }, {
+                        viewState.onError(it.message.toString())
+                    })
+            }, {
+                viewState.onError(it.message.toString())
+            })
+    }
+
+    fun getUriToBitmapCompress(bitmapOrigin: Bitmap?): Uri? {
+        return FileManager().getUriToBitmapCompress(bitmapOrigin)
     }
 
 }
