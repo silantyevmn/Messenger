304f5f0d7d4a8f8b3421c17db4bfb895ae42758d
silantyevmn <5992033@mail.ru>
2019-02-05 19:17:50 +0300

    в класс ChatMessage добавил поле id Изменил конструктор по умолчанию

--- app/src/main/java/ru/silantyevmn/mymessenger/model/entity/ChatMessage.kt
+++ app/src/main/java/ru/silantyevmn/mymessenger/model/entity/ChatMessage.kt
@@ -3,8 +3,8 @@ package ru.silantyevmn.mymessenger.model.entity
 import java.io.Serializable
 
 
-class ChatMessage(var id:String,val fromUid: String, val toUid: String, val text: String, val time: Long,var status:Int) : Serializable {
-    constructor() : this("","", "", "", -1,0)
+class ChatMessage(var uid:String, val id:String, val fromUid: String, val toUid: String, val typeMessage:String, var message: String, val time: Long, var status:Int) : Serializable {
+    constructor() : this("","","", "", "","", -1,0)
 
     override fun equals(other: Any?): Boolean {
         var tempChatMessage = other as ChatMessage
