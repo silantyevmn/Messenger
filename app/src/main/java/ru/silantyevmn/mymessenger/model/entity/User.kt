e4ba6ac07c42e36caeba689b2d8a818e5397577d
silantyevmn <5992033@mail.ru>
2019-02-05 19:12:05 +0300

    добавил equals

--- app/src/main/java/ru/silantyevmn/mymessenger/model/entity/User.kt
+++ app/src/main/java/ru/silantyevmn/mymessenger/model/entity/User.kt
@@ -5,4 +5,10 @@ import java.io.Serializable
 
 class User(val uid: String, val loginName: String, val imagePhotoUri: String): Serializable {
     constructor(): this("","","")
+
+    override fun equals(other: Any?): Boolean {
+        var tempUser = other as User
+        if(tempUser == null) return false
+        return uid.equals(tempUser.uid)
+    }
 }
